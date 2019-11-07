3b15b8dad19d746f651dc5f84061397549d69a6f
David Vasquez <David Vasquez@LAPTOP-2MP318IR>
2019-11-07 15:18:33 -0400

    Constructor, getters y setters de Cliente

--- Proyecto-P1/src/logico/Cliente.java
+++ Proyecto-P1/src/logico/Cliente.java
@@ -1,6 +1,60 @@
 package logico;
 
 public class Cliente {
-	//comentario
 	private String cedula;
+	private String nombre;
+	private String apellido;
+	private String direccion;
+	private String telefono;
+	
+	public Cliente(String cedula, String nombre, String apellido, String direccion, String telefono) {
+		super();
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
 }
