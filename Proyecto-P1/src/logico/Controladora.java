8830330ba2fc28dcf50919d0ea9c744fac275db4
David Vasquez <David Vasquez@LAPTOP-2MP318IR>
2019-11-10 11:09:33 -0400

    Creacion de unica instancia de clase Controladora

--- Proyecto-P1/src/logico/Controladora.java
+++ Proyecto-P1/src/logico/Controladora.java
@@ -1,5 +1,18 @@
 package logico;
 
-public class Controladora {
+import java.util.ArrayList;
 
+public class Controladora {
+	private static Controladora cont = null;
+	
+	private Controladora() {
+		super();
+	}
+	
+	public Controladora getInstance() {
+		if(cont==null) {
+			cont = new Controladora();
+		}
+		return cont;
+	}
 }
