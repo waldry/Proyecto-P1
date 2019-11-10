6364d7ed253f74789171e49ea2195ae005dea5cc
Waldry <waldrymaster@outlook.com>
2019-11-10 17:49:51 -0400

    Creacion de clases,getters and setters y constructor,unica estancia

---
+++ Proyecto-P1/src/logico/HistorialPago.java
@@ -0,0 +1,35 @@
+package logico;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HistorialPago {
+	private ArrayList<Factura> facturas;
+	private ArrayList<Cliente> clientes;
+	private Date fecha;
+	
+	public HistorialPago(ArrayList<Factura> facturas, ArrayList<Cliente> clientes, Date fecha) {
+		super();
+		this.facturas = facturas;
+		this.clientes = clientes;
+		this.fecha = fecha;
+	}
+	public ArrayList<Factura> getFacturas() {
+		return facturas;
+	}
+	public void setFacturas(ArrayList<Factura> facturas) {
+		this.facturas = facturas;
+	}
+	public ArrayList<Cliente> getClientes() {
+		return clientes;
+	}
+	public void setClientes(ArrayList<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+}
