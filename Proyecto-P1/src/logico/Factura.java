32b662cae6f843a9a544862add1bd6c68615fea4
Waldry <waldrymaster@outlook.com>
2019-11-10 17:28:17 -0400

    Agregando variables(services,activa), los setters and getters y modificando el constructor

--- Proyecto-P1/src/logico/Factura.java
+++ Proyecto-P1/src/logico/Factura.java
@@ -1,19 +1,40 @@
 package logico;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Factura {
 	private Cliente cliente;
-	private Plan planCliente;
+	private ArrayList<Servicio> services;
 	private Date fecha;
+	private Plan planCliente;
+	private boolean activa;
 	private float monto;
 	
-	public Factura(Cliente cliente, Plan planCliente, Date fecha, float monto) {
+	public Factura(Cliente cliente, Plan planCliente, Date fecha, float monto, ArrayList<Servicio> services, boolean activa) {
 		super();
 		this.cliente = cliente;
 		this.planCliente = planCliente;
 		this.fecha = fecha;
 		this.monto = monto;
+		this.services = services;
+		this.activa = activa;
+	}
+
+	public ArrayList<Servicio> getServices() {
+		return services;
+	}
+
+	public void setServices(ArrayList<Servicio> services) {
+		this.services = services;
+	}
+
+	public boolean isActiva() {
+		return activa;
+	}
+
+	public void setActiva(boolean activa) {
+		this.activa = activa;
 	}
 
 	public Cliente getCliente() {
