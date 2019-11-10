08eab7cf164d3ef85c329434f47108a3006284b9
David Vasquez <David Vasquez@LAPTOP-2MP318IR>
2019-11-10 19:19:33 -0400

    Variables estaticas y modificacion de constructor

--- Proyecto-P1/src/logico/HistorialPago.java
+++ Proyecto-P1/src/logico/HistorialPago.java
@@ -4,39 +4,51 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class HistorialPago {
-	private ArrayList<Factura> facturas;
-	private ArrayList<Cliente> clientes;
-	private Date fecha;
+	private static ArrayList<Factura> facturas;
+	private static ArrayList<Cliente> clientes;
+	private static Date fecha;
 	private static HistorialPago historiaPago = null;
 	
 	private HistorialPago() {
 		super();
-		this.facturas = facturas;
-		this.clientes = clientes;
-		this.fecha = fecha;
 	}
-	public ArrayList<Factura> getFacturas() {
-		return facturas;
-	}
-	public void setFacturas(ArrayList<Factura> facturas) {
-		this.facturas = facturas;
-	}
-	public ArrayList<Cliente> getClientes() {
-		return clientes;
-	}
-	public void setClientes(ArrayList<Cliente> clientes) {
-		this.clientes = clientes;
-	}
-	public Date getFecha() {
-		return fecha;
-	}
-	public void setFecha(Date fecha) {
-		this.fecha = fecha;
-	}
+
 	public HistorialPago getInstance() {
 		if(historiaPago==null) {
 			historiaPago = new HistorialPago();
 		}
 		return historiaPago;
+	}
+
+	public static ArrayList<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public static void setFacturas(ArrayList<Factura> facturas) {
+		HistorialPago.facturas = facturas;
+	}
+
+	public static ArrayList<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public static void setClientes(ArrayList<Cliente> clientes) {
+		HistorialPago.clientes = clientes;
+	}
+
+	public static Date getFecha() {
+		return fecha;
+	}
+
+	public static void setFecha(Date fecha) {
+		HistorialPago.fecha = fecha;
+	}
+
+	public static HistorialPago getHistoriaPago() {
+		return historiaPago;
+	}
+
+	public static void setHistoriaPago(HistorialPago historiaPago) {
+		HistorialPago.historiaPago = historiaPago;
 	}
 }
