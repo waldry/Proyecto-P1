9af1b6c262b6bc74a90d3070dcda3ce9a7db4597
David Vasquez <David Vasquez@LAPTOP-2MP318IR>
2019-11-14 00:18:08 -0400

    Creacion de ventana RegistarCliente

---
+++ Proyecto-P1/src/visual/RegistrarCliente.java
@@ -0,0 +1,57 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class RegistrarCliente extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			RegistrarCliente dialog = new RegistrarCliente();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public RegistrarCliente() {
+		setTitle("Registro de Cliente");
+		setResizable(false);
+		setBounds(100, 100, 579, 358);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton btnregistrar = new JButton("Registrar");
+				btnregistrar.setActionCommand("OK");
+				buttonPane.add(btnregistrar);
+				getRootPane().setDefaultButton(btnregistrar);
+			}
+			{
+				JButton btncancelar = new JButton("Cancelar");
+				btncancelar.setActionCommand("Cancel");
+				buttonPane.add(btncancelar);
+			}
+		}
+	}
+
+}
