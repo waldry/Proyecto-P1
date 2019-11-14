d59658fb0d3ba1376c830fbb664198d9caf7f69a
David Vasquez <David Vasquez@LAPTOP-2MP318IR>
2019-11-14 00:15:07 -0400

    Barra de menu agregada

--- Proyecto-P1/src/visual/Principal.java
+++ Proyecto-P1/src/visual/Principal.java
@@ -7,6 +7,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Principal extends JFrame {
 
@@ -39,11 +42,25 @@ public class Principal extends JFrame {
 		setBounds(100, 100, 450, 300);
 		dim = super.getToolkit().getScreenSize();
 		super.setSize(dim.width, dim.height-100);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Clientes");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmRegistrarCliente = new JMenuItem("Registrar Cliente");
+		mnNewMenu.add(mntmRegistrarCliente);
+		
+		JMenuItem mntmListadoDeClientes = new JMenuItem("Listado de Clientes");
+		mnNewMenu.add(mntmListadoDeClientes);
+		
+		JMenu mnPlanesYServicios = new JMenu("Planes y Servicios");
+		menuBar.add(mnPlanesYServicios);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
 	}
-
 }
