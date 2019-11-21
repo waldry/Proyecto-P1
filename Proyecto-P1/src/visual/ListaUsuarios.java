26ee9e875cedadcc62a89bfe33b32f2a214a96e5
David Vasquez <David Vasquez@LAPTOP-2MP318IR>
2019-11-21 18:27:32 -0400

    ListaUsuarios creada y retoques a ListaClientes

--- Proyecto-P1/src/visual/ListaUsuarios.java
+++ Proyecto-P1/src/visual/ListaUsuarios.java
@@ -7,24 +7,25 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 
-import logico.Cliente;
 import logico.Controladora;
+import logico.Personal;
 
 import javax.swing.border.EtchedBorder;
 import java.awt.Color;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 
-public class ListaClientes extends JDialog {
+public class ListaUsuarios extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	private JTable table;
@@ -32,14 +33,14 @@ public class ListaClientes extends JDialog {
 	private Object[] row;
 	private JButton btneliminar;
 	private JButton btnModificar;
-	private String cedula = "";
+	private String username = "";
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		try {
-			ListaClientes dialog = new ListaClientes();
+			ListaUsuarios dialog = new ListaUsuarios();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -50,21 +51,20 @@ public class ListaClientes extends JDialog {
 	/**
 	 * Create the dialog.
 	 */
-	public ListaClientes() {
-		setTitle("Lista de Clientes");
+	public ListaUsuarios() {
 		setResizable(false);
-		setBounds(100, 100, 545, 360);
+		setBounds(100, 100, 528, 329);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Listado de clientes", TitledBorder.LEADING, TitledBorder.TOP, null, Color.BLACK));
+		contentPanel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Listado de Usuarios", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		setLocationRelativeTo(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 22, 521, 253);
+		scrollPane.setBounds(10, 34, 432, 226);
 		contentPanel.add(scrollPane);
 		model = new DefaultTableModel();
-		String[] header = {"Cédula", "Nombre", "Apellido", "Dirección", "Teléfono", "Activo"};
+		String[] header = {"Username", "Nombre", "Apellido", "Oficina", "Tipo", "lastlogin"};
 		model.setColumnIdentifiers(header);
 		table = new JTable();
 		table.addMouseListener(new MouseAdapter() {
@@ -74,7 +74,7 @@ public class ListaClientes extends JDialog {
 					int index = table.getSelectedRow();
 					btneliminar.setEnabled(true);
 					btnModificar.setEnabled(true);
-					cedula = String.valueOf(table.getValueAt(index, 0));
+					username = String.valueOf(table.getValueAt(index, 0));
 				}
 			}
 		});
@@ -85,30 +85,29 @@ public class ListaClientes extends JDialog {
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			
-			btnModificar = new JButton("Modificar");
-			btnModificar.setEnabled(false);
-			buttonPane.add(btnModificar);
 			{
-				btneliminar = new JButton("Eliminar");
-				btneliminar.setEnabled(false);
-				btneliminar.addActionListener(new ActionListener() {
+				JButton btnModificar = new JButton("Modificar");
+				btnModificar.setActionCommand("OK");
+				buttonPane.add(btnModificar);
+				getRootPane().setDefaultButton(btnModificar);
+			}
+			{
+				JButton btnEliminar = new JButton("Eliminar");
+				btnEliminar.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						if(!cedula.equals("")) {
-							Cliente aux = Controladora.getInstance().findClienteById(cedula);
-							int option = JOptionPane.showConfirmDialog(null, "Está seguro que desea eliminar el Cliente: " + aux.getNombre(),"Información",JOptionPane.WARNING_MESSAGE);
-								if(option == JOptionPane.OK_OPTION) {
-									Controladora.getInstance().eliminarCliente(aux);
-									loadClientes();
-									btneliminar.setEnabled(false);
-									btnModificar.setEnabled(false);
-								}
+						if(!username.equals("")) {
+							Personal aux = Controladora.getInstance().findPersonalByUsername(username);
+							int option = JOptionPane.showConfirmDialog(null, "Está seguro que desea eliminar el Usuario: " + aux.getNombre(),"Información",JOptionPane.WARNING_MESSAGE);
+							if(option == JOptionPane.OK_OPTION) {
+								Controladora.getInstance().eliminarUsuario(aux);
+								loadUsuarios();
+								btneliminar.setEnabled(false);
+								btnModificar.setEnabled(false);
+							}
 						}
 					}
 				});
-				btneliminar.setActionCommand("OK");
-				buttonPane.add(btneliminar);
-				getRootPane().setDefaultButton(btneliminar);
+				buttonPane.add(btnEliminar);
 			}
 			{
 				JButton btncancelar = new JButton("Cancelar");
@@ -121,22 +120,18 @@ public class ListaClientes extends JDialog {
 				buttonPane.add(btncancelar);
 			}
 		}
-		loadClientes();
+		loadUsuarios();
 	}
-	public void loadClientes() {
+	public void loadUsuarios() {
 		model.setRowCount(0);
 		row = new Object[model.getColumnCount()];
-		for (int i = 0; i < Controladora.getInstance().getClientes().size(); i++) {
-			row[0] = Controladora.getInstance().getClientes().get(i).getCedula();
-			row[1] = Controladora.getInstance().getClientes().get(i).getNombre();
-			row[2] = Controladora.getInstance().getClientes().get(i).getApellido();
-			row[3] = Controladora.getInstance().getClientes().get(i).getDireccion();
-			row[4] = Controladora.getInstance().getClientes().get(i).getTelefono();
-			if(Controladora.getInstance().getClientes().get(i).isActivo() == true) {
-				row[5] = "Si";
-			}else {
-				row[5] = "No";
-			}
+		for (int i = 0; i < Controladora.getInstance().getEmpleados().size(); i++) {
+			row[0] = Controladora.getInstance().getEmpleados().get(i).getUsername();
+			row[1] = Controladora.getInstance().getEmpleados().get(i).getNombre();
+			row[2] = Controladora.getInstance().getEmpleados().get(i).getApellido();
+			row[3] = Controladora.getInstance().getEmpleados().get(i).getOficina();
+			row[4] = Controladora.getInstance().getEmpleados().get(i).getTipo();
+			row[5] = Controladora.getInstance().getEmpleados().get(i).getLastLogin();
 			model.addRow(row);
 		}
 	}
