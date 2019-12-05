dc7a2cea80b84244cb3ab180316ec619dd2b72ac
David Vasquez <David Vasquez@LAPTOP-2MP318IR>
2019-12-05 10:38:26 -0400

    Creacion de PagoFactura

--- Proyecto-P1/src/visual/PagoFactura.java
+++ Proyecto-P1/src/visual/PagoFactura.java
@@ -11,13 +11,21 @@ import javax.swing.JLabel;
 import javax.swing.border.TitledBorder;
 import javax.swing.text.MaskFormatter;
 
+import logico.Cliente;
+import logico.Contrato;
+import logico.Controladora;
+
 import java.awt.Color;
 import javax.swing.border.EtchedBorder;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.JFormattedTextField;
 import javax.swing.JTextField;
+import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 
 public class PagoFactura extends JDialog {
 
@@ -26,7 +34,10 @@ public class PagoFactura extends JDialog {
 	private JTextField txtApellido;
 	private JTextField txtDireccion;
 	private JTextField txtTelefono;
+	private DefaultListModel<String> idContratos = new DefaultListModel<String>();
+	private JList<String> contratos;
 	private JFormattedTextField ftxtCedula;
+	private Cliente clientelito;
 	private MaskFormatter mascara() {
 		MaskFormatter mascara = new MaskFormatter();
 		try {
@@ -57,14 +68,17 @@ public class PagoFactura extends JDialog {
 	public PagoFactura() {
 		setTitle("Pago de Factura");
 		setResizable(false);
-		setBounds(100, 100, 784, 411);
+		setBounds(100, 100, 633, 276);
 		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(new Color(240, 248, 255));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		setLocationRelativeTo(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Informaci\u00F3n de Cliente", TitledBorder.LEADING, TitledBorder.TOP, null, Color.BLACK));
+		panel.setBackground(new Color(240, 248, 255));
+		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Informaci\u00F3n de Cliente", TitledBorder.LEADING, TitledBorder.TOP, null, Color.BLACK));
 		panel.setBounds(10, 10, 331, 189);
 		contentPanel.add(panel);
 		panel.setLayout(null);
@@ -114,16 +128,55 @@ public class PagoFactura extends JDialog {
 		txtTelefono.setColumns(10);
 		
 		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for (Cliente client : Controladora.getInstance().getClientes()) {
+					if(ftxtCedula.getText().equals(client.getCedula())) {
+						JOptionPane.showMessageDialog(null, "Cliente existente", "Notificacion", JOptionPane.INFORMATION_MESSAGE);
+						clientelito = client;
+						txtNombre.setText(client.getNombre());
+						txtApellido.setText(client.getApellido());
+						txtDireccion.setText(client.getDireccion());
+						txtTelefono.setText(client.getTelefono());
+						txtNombre.setEditable(false);
+						txtApellido.setEditable(false);
+						txtDireccion.setEditable(false);
+						txtTelefono.setEditable(false);
+						updateContratos();
+					}else {
+						JOptionPane.showMessageDialog(null, "Cédula inválida. Por favor escriba la cédula correctamente", "Notificación", JOptionPane.INFORMATION_MESSAGE);
+						ftxtCedula.setText("");
+					}
+				}
+			}
+		});
 		btnBuscar.setIcon(new ImageIcon(PagoFactura.class.getResource("/recursos/lupa16.png")));
 		btnBuscar.setBounds(213, 14, 108, 21);
 		panel.add(btnBuscar);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(240, 248, 255));
+		panel_1.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Contrato(s) del Cliente", TitledBorder.LEADING, TitledBorder.TOP, null, Color.BLACK));
+		panel_1.setBounds(351, 10, 268, 189);
+		contentPanel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		contratos = new JList<>(idContratos);
+		contratos.setEnabled(false);
+		contratos.setBounds(10, 25, 246, 154);
+		panel_1.add(contratos);
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBackground(new Color(240, 248, 255));
 			buttonPane.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				JButton btnPago = new JButton("Pagar");
+				btnPago.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+					}
+				});
 				btnPago.setActionCommand("OK");
 				buttonPane.add(btnPago);
 				getRootPane().setDefaultButton(btnPago);
@@ -137,6 +190,15 @@ public class PagoFactura extends JDialog {
 				});
 				btnCancelar.setActionCommand("Cancel");
 				buttonPane.add(btnCancelar);
+			}
+		}
+		updateContratos();
+	}
+	public void updateContratos() {
+		idContratos.clear();
+		for (Contrato contract : Controladora.getInstance().getContratos()) {
+			if(contract.getClient() == clientelito) {
+				idContratos.addElement(contract.getId()+". Precio: "+contract.getTotal());
 			}
 		}
 	}
