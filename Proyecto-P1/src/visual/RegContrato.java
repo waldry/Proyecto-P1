d457e1d21676cc24d85145799595ac7233786d54
David Vasquez <David Vasquez@LAPTOP-2MP318IR>
2019-12-05 02:54:32 -0400

    Estetica y valores de Importe de Contrato en RegContrato

--- Proyecto-P1/src/visual/RegContrato.java
+++ Proyecto-P1/src/visual/RegContrato.java
@@ -39,6 +39,9 @@ import javax.swing.JOptionPane;
 import java.awt.Font;
 import javax.swing.UIManager;
 import java.awt.Color;
+import java.awt.Toolkit;
+import javax.swing.border.EtchedBorder;
+import javax.swing.ImageIcon;
 
 public class RegContrato extends JDialog {
 
@@ -57,6 +60,8 @@ public class RegContrato extends JDialog {
 	DefaultListModel<String> dbPlanes = new DefaultListModel<>();
 	DefaultListModel<String> planesSelected = new DefaultListModel<String>();
 	private JLabel subtotal_lbl;
+	private JLabel itbis_lbl;
+	private JLabel total_lbl;
 	private String idContrato = "C-"+Controladora.getInstance().getGenCodContrato();
 	private MaskFormatter mascara() {
 		MaskFormatter mascara = new MaskFormatter();
@@ -86,16 +91,19 @@ public class RegContrato extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegContrato() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(RegContrato.class.getResource("/recursos/check-list.png")));
 		setTitle("Registro de contrato");
 		setResizable(false);
 		setBounds(100, 100, 1038, 702);
 		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(new Color(240, 248, 255));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Informacion - Cliente", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBackground(new Color(240, 248, 255));
+		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Informacion - Cliente", TitledBorder.LEADING, TitledBorder.TOP, null, Color.BLACK));
 		panel.setBounds(20, 11, 368, 221);
 		contentPanel.add(panel);
 		panel.setLayout(null);
@@ -158,6 +166,7 @@ public class RegContrato extends JDialog {
 		panel.add(lblTelefono);
 		
 		activo_chbx= new JCheckBox("Activo");
+		activo_chbx.setBackground(new Color(240, 248, 255));
 		activo_chbx.setBounds(10, 178, 68, 23);
 		panel.add(activo_chbx);
 		
@@ -187,12 +196,14 @@ public class RegContrato extends JDialog {
 		tel_txt.setColumns(10);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informacion - Plan", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_1.setBackground(new Color(240, 248, 255));
+		panel_1.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Informacion - Plan", TitledBorder.LEADING, TitledBorder.TOP, null, Color.BLACK));
 		panel_1.setBounds(20, 243, 992, 376);
 		contentPanel.add(panel_1);
 		panel_1.setLayout(null);
 		
 		main = new JList<>(dbPlanes);
+		main.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		main.setBounds(86, 50, 268, 315);
 		panel_1.add(main);
 		updatePlanes();
@@ -220,12 +231,14 @@ public class RegContrato extends JDialog {
 					idPlanes.add(id);
 					aux = aux+Float.parseFloat(costo); 
 					subtotal_lbl.setText(String.valueOf(aux));
+					itbis_lbl.setText(String.valueOf(aux*0.28));
+					total_lbl.setText(String.valueOf(aux+(0.28*aux)));
 					}
 				}
 			}
 		});
 		agregar_btn.setFont(new Font("Tahoma", Font.BOLD, 20));
-		agregar_btn.setBounds(440, 50, 111, 49);
+		agregar_btn.setBounds(440, 84, 111, 49);
 		panel_1.add(agregar_btn);
 		
 		JButton eliminar_btn = new JButton("<-");
@@ -245,6 +258,7 @@ public class RegContrato extends JDialog {
 						idPlanes.remove(id);
 						aux = aux-Float.parseFloat(costo);
 						subtotal_lbl.setText(String.valueOf(Math.abs(aux)));
+						
 					}
 				}
 				if (second.getModel().getSize()==0) {
@@ -253,10 +267,11 @@ public class RegContrato extends JDialog {
 			}
 		});
 		eliminar_btn.setFont(new Font("Tahoma", Font.BOLD, 20));
-		eliminar_btn.setBounds(440, 289, 111, 49);
+		eliminar_btn.setBounds(440, 258, 111, 49);
 		panel_1.add(eliminar_btn);
 		
 		second= new JList<>(planesSelected);
+		second.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		second.setBounds(637, 50, 268, 315);
 		panel_1.add(second);
 		
@@ -266,36 +281,47 @@ public class RegContrato extends JDialog {
 		panel_1.add(lblPlanesEnEl);
 		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(new TitledBorder(null, "Importes del Contrato", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_2.setBounds(680, 11, 319, 221);
+		panel_2.setBackground(new Color(240, 248, 255));
+		panel_2.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Importes del Contrato", TitledBorder.LEADING, TitledBorder.TOP, null, Color.BLACK));
+		panel_2.setBounds(398, 11, 449, 86);
 		contentPanel.add(panel_2);
 		panel_2.setLayout(null);
 		
 		JLabel lblSubtotal = new JLabel("Subtotal:");
-		lblSubtotal.setBounds(10, 34, 56, 14);
+		lblSubtotal.setBounds(19, 34, 56, 14);
 		panel_2.add(lblSubtotal);
 		
 		JLabel lblItbis = new JLabel("ITBIS:");
-		lblItbis.setBounds(10, 59, 46, 14);
+		lblItbis.setBounds(169, 34, 46, 14);
 		panel_2.add(lblItbis);
 		
 		JLabel lblTotal= new JLabel("Total:");
-		lblTotal.setBounds(10, 88, 46, 14);
+		lblTotal.setBounds(309, 34, 46, 14);
 		panel_2.add(lblTotal);
 		
 		subtotal_lbl = new JLabel("0.00");
-		subtotal_lbl.setBounds(76, 34, 46, 14);
+		subtotal_lbl.setBackground(new Color(255, 255, 255));
+		subtotal_lbl.setBounds(94, 34, 56, 14);
 		panel_2.add(subtotal_lbl);
 		
-		JLabel itbis_lbl = new JLabel("0.00");
-		itbis_lbl.setBounds(76, 59, 46, 14);
+		itbis_lbl = new JLabel("0.00");
+		itbis_lbl.setBackground(new Color(255, 255, 255));
+		itbis_lbl.setBounds(234, 34, 56, 14);
 		panel_2.add(itbis_lbl);
 		
-		JLabel total_lbl = new JLabel("0.00");
-		total_lbl.setBounds(76, 88, 46, 14);
+		total_lbl = new JLabel("0.00");
+		total_lbl.setBackground(new Color(255, 255, 255));
+		total_lbl.setBounds(374, 34, 56, 14);
 		panel_2.add(total_lbl);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(RegContrato.class.getResource("/recursos/expediente128.png")));
+		label.setBounds(871, 31, 141, 181);
+		contentPanel.add(label);
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+			buttonPane.setBackground(new Color(240, 248, 255));
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
