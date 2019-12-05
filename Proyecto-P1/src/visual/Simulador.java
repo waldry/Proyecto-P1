8fc2de7716c2a29a464a8e3de45d2f231af17de1
waldry <waldry@DESKTOP-UA2OP50>
2019-12-04 22:02:06 -0400

    Implementando bases para simular facturas.

--- Proyecto-P1/src/visual/Simulador.java
+++ Proyecto-P1/src/visual/Simulador.java
@@ -7,10 +7,18 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+import javax.swing.JList;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Simulador extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -29,11 +37,49 @@ public class Simulador extends JDialog {
 	 * Create the dialog.
 	 */
 	public Simulador() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 470, 502);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ID Contrato: ");
+		lblNewLabel.setBounds(10, 22, 90, 14);
+		contentPanel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(86, 19, 86, 20);
+		contentPanel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(182, 18, 89, 23);
+		contentPanel.add(btnBuscar);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Informacion - Contrato", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(20, 47, 424, 287);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblCliente = new JLabel("Cliente: ");
+		lblCliente.setBounds(10, 36, 59, 14);
+		panel.add(lblCliente);
+		
+		textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		textField_1.setBounds(64, 33, 86, 20);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JList list = new JList();
+		list.setEnabled(false);
+		list.setBounds(10, 80, 151, 196);
+		panel.add(list);
+		
+		JLabel lblServicios = new JLabel("Servicios");
+		lblServicios.setBounds(62, 61, 66, 14);
+		panel.add(lblServicios);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -45,11 +91,15 @@ public class Simulador extends JDialog {
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
+				JButton cancelButton = new JButton("Cancelar");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
 	}
-
 }
